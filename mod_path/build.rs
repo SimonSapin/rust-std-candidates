b0b364e101b9014008b1e8719f8e2efc9245e991
Simon Sapin <simon.sapin@exyr.org>
2015-03-25 19:02:17 +0100

    Upgrade to rustc 1.0.0-nightly (123a754cb 2015-03-24) (built 2015-03-25)

--- mod_path/build.rs
+++ mod_path/build.rs
@@ -1,4 +1,4 @@
-#![feature(io, path)]
+#![feature(convert)]
 
 use std::env;
 use std::fs::File;
@@ -6,7 +6,7 @@ use std::io::Write;
 use std::path::PathBuf;
 
 fn main() {
-    let dst = PathBuf::new(&env::var("OUT_DIR").unwrap());
+    let dst = PathBuf::from(&env::var("OUT_DIR").unwrap());
     let mut f = File::create(&dst.join("hello.rs")).unwrap();
     f.write_all(b"
         pub const SIX_BY_NINE: u32 = 0o42;
